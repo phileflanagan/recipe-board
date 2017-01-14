e16903f9b839ac4edb7304e89f788aa663a3040e
Phil Flanagan <phileflanagan@gmail.com>
2017-01-14 15:44:16 -0500

    Image preloader added

--- mealplanner/src/components/Recipes.js
+++ mealplanner/src/components/Recipes.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Preload } from 'react-preload';
 
 import Recipe from './Recipe';
 import samples from '../sample-items';
@@ -6,17 +7,40 @@ import samples from '../sample-items';
 import '../css/Recipes.css';
 
 class Recipes extends Component {
+  _handleImageLoadError = () => {
+    return;
+  }
+
+  _handleImageLoadSuccess = () => {
+    return;
+  }
+
   render() {
+    const loadingIndicator = <div>Loading...</div>;
+    const images = Object.keys(samples).map(key => samples[key].image);
+
     return (
       <div className="container">
-        <div className="gallery">
-          {Object.keys(samples)
-            .map(key => <Recipe
-                          item={samples[key]}
-                          key={key}
-                          index={key} />
-            )}
-        </div>
+
+        <Preload
+          loadingIndicator={loadingIndicator}
+          images={images}
+          autoResolveDelay={5000}
+          onError={this._handleImageLoadError}
+          onSuccess={this._handleImageLoadSuccess}
+          resolveOnError={true}
+          mountChildren={true}
+          >
+          <div className="gallery">
+            {Object.keys(samples)
+              .map(key => <Recipe
+                            item={samples[key]}
+                            key={key}
+                            index={key} />
+              )}
+          </div>
+        </Preload>
+
       </div>
     );
   }
