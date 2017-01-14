db88fbd8b92b26f8293dda6c5361b168131f64a5
Phil Flanagan <phileflanagan@gmail.com>
2017-01-14 17:23:48 -0500

    Add loader to about page

--- mealplanner/src/components/About.js
+++ mealplanner/src/components/About.js
@@ -1,25 +1,41 @@
 import React from 'react';
+import { Preload } from 'react-preload';
+
+import Loader from './Loader';
 import '../css/About.css';
 
 const About = () => {
+  const loadingIndicator = Loader();
+  const images = ['https://s3-us-west-2.amazonaws.com/philinthekitchen/me.jpg'];
+
   return (
     <div className="about">
-      <section className="whoami">
-      <div className="container">
-        <div className="info-about-me">
-          <img src="https://s3-us-west-2.amazonaws.com/philinthekitchen/me.jpg" alt="Phil Flanagan"/>
-          <h2>Who Am I?</h2>
-          <p>Hi, my name is Phil Flanagan. I adhere to a plant-based diet, which combines my three big passions: nutrition, cooking, and eating. Beyond the kitchen, I am a Web Developer.</p>
-        </div>
-      </div>
-      </section>
-
-      <section className="site-about">
+      <Preload
+        loadingIndicator={loadingIndicator}
+        images={images}
+        autoResolveDelay={5000}
+        resolveOnError={true}
+        mountChildren={true}>
+      <div className="gallery">
+        <section className="whoami">
         <div className="container">
-          <h2>What is this Site About?</h2>
-          <p> This site is meant to share healthful plant based recipes. While not every recipe on here is salt, oil, and sugar free and a few of the recipes may contain these items or other processed and refined ingredients, I try to make whole plant foods that corner stone of my diet. I also understanding the human element.</p>
+          <div className="info-about-me">
+            <img src="https://s3-us-west-2.amazonaws.com/philinthekitchen/me.jpg" alt="Phil Flanagan"/>
+            <h2>Who Am I?</h2>
+            <p>Hi, my name is Phil Flanagan. I adhere to a plant-based diet, which combines my three big passions: nutrition, cooking, and eating. Beyond the kitchen, I am a Web Developer.</p>
+          </div>
         </div>
-      </section>
+        </section>
+
+        <section className="site-about">
+          <div className="container">
+            <h2>What is this Site About?</h2>
+            <p> This site is meant to share healthful plant based recipes. While not every recipe on here is salt, oil, and sugar free and a few of the recipes may contain these items or other processed and refined ingredients, I try to make whole plant foods that corner stone of my diet. I also understanding the human element.</p>
+          </div>
+        </section>
+      </div>
+      </Preload>
+
     </div>
   );
 }
