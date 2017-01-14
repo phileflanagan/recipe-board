2a8639566bf40a79d9947b562bf93eded17d710d
Phil Flanagan <phileflanagan@gmail.com>
2017-01-14 03:42:34 -0500

    start restructuring cards for styling update

--- mealplanner/src/components/Recipe.js
+++ mealplanner/src/components/Recipe.js
@@ -22,30 +22,27 @@ class Recipe extends Component {
   }
 
   render() {
-    const { name, image, desc, likes, commentCount, comments, tags, recipe, recipeVideo } = this.props.item;
+    const { name, image, desc, likes, commentCount, tags} = this.props.item;
 
     return (
-      <div className="recipe-card card">
-        <div className="thumb">
-          <Link to={`/recipe/${this.props.index}`}><img src={image} alt={name}/></Link>
-        </div>
-        <div className="desc">
-          <h3>
-            <Link to={`/recipe/${this.props.index}`}>{name}</Link>
-          </h3>
-          <p>{desc}</p>
-          {this.renderTags(tags)}
+      <div className="recipe-card">
+        <div className="recipe-card-content">
+            <Link to={`/recipe/${this.props.index}`}><img src={image} alt={name}/></Link>
+            <h3>
+              <Link to={`/recipe/${this.props.index}`}>{name}</Link>
+            </h3>
+            <p>{desc}</p>
+            {this.renderTags(tags)}
             <div className="likes">
               <Link to={`/recipe/${this.props.index}`} className="card-btn">
                 <span className="count">{commentCount}</span>
                 <span className="count-icon">comments</span>
               </Link>
-              <button>
+              <a href="#" className="card-btn">
                 <span className="count">{likes}</span>
                 <span className="count-icon">saves</span>
-              </button>
+              </a>
             </div>
-            {console.log('use these guys:', comments, recipe, recipeVideo)}
         </div>
       </div>
     );
